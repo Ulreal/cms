f6c00291bb0a0f827b535187ef7a8ca8a481d937
Quentin BETTOUM <quentin.bettoum@epitech.eu>
2016-05-31 12:19:52 +0200

    Layout Forum

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,7 @@
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
                     @if (Auth::check())
+                        <li>{{link_to_route('post.index', 'Forum')}}</li>
                         <li>{{link_to_route('chat.index', 'Chat')}}</li>
                         <li>{{link_to_route('news.index', 'News')}}</li>
                     @endif
